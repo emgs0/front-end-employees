fb9f530c22a76e0858496e3822f9e699126ea8e4
Eduardo Garza Sanchez <e-emgarza@neoris.com>
2021-08-25 00:19:44 -0500

    crud completo (angular + springboot)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"empleados", component: EmployeeListComponent},
+  {path:'crear-empleado',component:CreateEmployeeComponent},
+  {path: 'actualizar-empleado/:id', component:UpdateEmployeeComponent},
+  {path: "detalles-empleado/:id",component:EmployeeDetailsComponent},
+  {path:"", redirectTo: "empleados", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
